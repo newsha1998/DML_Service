e0f5b1904a2eeb05e231088f1f8d76f1b44378ba
negarmajma <68001084+negarmajma@users.noreply.github.com>
2021-01-20 13:50:45 +0330

    First commin

--- SQL_SDK/run.py
+++ SQL_SDK/run.py
@@ -5,22 +5,51 @@ from User import User
 from UserManager import UserManager
 
 
-def run_command(dir, command):
+
+def run_command(dir, command,df):
     if not os.path.exists(dir):
         os.mkdir(dir)
-    if command == 'sql':
-        cmd = input()
-        sdk.run_sql(dir, cmd)
+    if command == 'Sql':
+        cmd = input("wriet sql command:")
+        sdk.run_sql(cmd,df)
     if command == 'show':
         pass
     if command == 'create':
         pass
+      
     if command == 'description':
         pass
-    if command == 'import':
-        path = input()
-        sdk.importt(dir, path)
-    pass
+    if command == 'Import':
+        cmdpath = input("Path file:")
+        cmdformat=input("file format(csv/json):")
+        cmdheader=input("file has a header column (true/false):")
+        cmdname=input("tabele name:")
+        df= sdk.importt(cmdpath,cmdname,cmdformat,cmdheader)
+        return df
+    if command=='ShowColumn':
+        sdk.ShowColumn(df)
+    if command=='ChangeColumnTpye':
+        cmdColumnName=input('Enter column name:')
+        cmdColumntype=input('Enter new column type:')
+        df=sdk.ChangeColumntype(df,cmdColumnName,cmdColumntype)
+        return df
+    if command=='ChangeColumnName':
+        cmdColumnName=input('Enter old column name:')
+        cmdColumnNName=input('Enter new column name:')
+        df=sdk.ChangeColumnName(df,cmdColumnName,cmdColumnNName)
+        return df
+    if command=='AddColumn':
+        cmddfName=input('Enter table name:')
+        cmdColumnName=input('Enter column name:')
+        cmdColumnType=input('Enter column type:')
+        df=sdk.AddColumn(df,cmddfName,cmdColumnName,cmdColumnType)
+        return df
+    if command=='DropNull':
+        df=sdk.dropNull(df)
+        return df
+    if command=='Save':
+        cmd=input('Enter path and output file neme:')
+        sdk.Save(df,cmd)
 
 
 if __name__ == "__main__":
@@ -46,8 +75,11 @@ if __name__ == "__main__":
             user = User(user, pas)
     print('Welcome', user.username)
 
+    print("enter a command(Import/Sql/ShowColumn/ChangeColumnTpye/ChangeColumnName/AddColumn/DropNull/Save/q")
     command = input()
+    df =None
     while command != 'q':
-        run_command(user.dir, command)
+        df=run_command(user.dir, command,df)
+        print("enter a command(Import/Sql/ShowColumn/ChangeColumnTpye/ChangeColumnName/AddColumn/DropNull/Save/q")
         command = input()
     um.close()
--- SQL_SDK/sdk.py
+++ SQL_SDK/sdk.py
@@ -1,18 +1,80 @@
-def save(dir, dataframe):
-    pass
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import lit
+
+#spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder \
+    .master("local") \
+    .appName("DML app") \
+    .config("header", "true") \
+    .getOrCreate() 
+
+def run_sql(sqlcommand,dataframe):
+    spark.sql(sqlcommand).show()
+
+def importt(path,DataframName='Test',type='csv',header="true"):
+    
+    if type=="csv" : 
+        df =spark.read.format("csv").option("header",header).load(path)  
+        df.createOrReplaceTempView(DataframName)
+        print("=== Print out schema ===")
+        df.printSchema()
+        return df
+    elif type=="json":
+        df =spark.read.format("json").option("header",header).load(path)  
+        df.createOrReplaceTempView(DataframName)
+        print("=== Print out schema ===")
+        df.printSchema()
+        return df
+
+def ShowColumn(dataframe):   
+    print("Number of the column:" + str(len(dataframe.columns)) )
+    df_list=dataframe.dtypes 
+    for i in df_list:
+          print(i)
 
 
-def exportt(path):
-    pass
+def ChangeColumntype(dataframe,columnName,newtype):
+    if newtype=="Float":
+        dataframe=dataframe.withColumn(columnName,dataframe[columnName].cast(FloatType()))
+        print("done")
+        return dataframe
+    elif newtype=="String":
+        dataframe=dataframe.withColumn(columnName,dataframe[columnName].cast(StringType()))
+        print("done")
+        return dataframe
+    elif newtype=="Integer":
+        dataframe=dataframe.withColumn(columnName,dataframe[columnName].cast(IntegerType()))
+        print("done")
+        return dataframe
+    
+
+def ChangeColumnName(dataframe,columnNameOld,ColumnNameNew):
+    dataframe=dataframe.withColumnRenamed(columnNameOld,ColumnNameNew)
+    print("done")
+    return dataframe
 
 
-def run_sql(path):
-    pass
+def AddColumn(dataframe,dataframeName='Test',columnName='test1',columnType="String",defaultValue=None):
+    
+    if columnType=="String":
+        dataframe=dataframe.withColumn(columnName,lit(defaultValue).cast(StringType()))
+        dataframe.createOrReplaceTempView(dataframeName)
+        print("done")
+        return dataframe
+    elif columnType=="Integer":
+        dataframe=dataframe.withColumn(columnName,lit(defaultValue).cast(IntegerType()))
+        dataframe.createOrReplaceTempView(dataframeName)
+        print("done")
+        return dataframe
+    elif columnType=="Float":
+        dataframe=dataframe.withColumn(columnName,lit(defaultValue).cast(FloatType()))
+        dataframe.createOrReplaceTempView(dataframeName)
+        print("done")
+        return dataframe
 
-
-def create(path):
-    pass
-
-
-def importt(dir, path):
-    pass
+def dropNull(dataframe):
+    return dataframe.na.drop().show()
+  
+def Save(dataframe,outpath):
+    dataframe.write.csv(outpath)
